e762b03be4f17ed1e50d743c1219e98914f03980
liyiming.709 <liyiming.709@bytedance.com>
2026-04-20 12:42:22 +0800

    更新服务器地址与相关配置文件
    Change-Id: Ic6ca06da757fbe1720716fe774bfb313fe23931d

--- start_remote.sh
+++ start_remote.sh
@@ -2,14 +2,14 @@
 set timeout -1
 set password [lindex $argv 0]
 
-spawn ssh -o StrictHostKeyChecking=no lym@47.93.224.109 "export PATH=/usr/local/bin:\$PATH && cd /home/lym/stocks/backend && (pm2 delete stock-backend || true) && pm2 start dist/index.js --name stock-backend"
+spawn ssh -o StrictHostKeyChecking=no -p 14126 root@103.242.3.87 "export PATH=/usr/local/bin:\$PATH && cd /opt/stocks/backend && (pm2 delete stock-backend || true) && NODE_ENV=production pm2 start dist/index.js --name stock-backend"
 expect {
     "*yes/no*" { send "yes\r"; exp_continue }
     "*assword:*" { send "$password\r"; exp_continue }
     eof
 }
 
-spawn ssh -o StrictHostKeyChecking=no lym@47.93.224.109 "export PATH=/usr/local/bin:\$PATH && cd /home/lym/stocks/frontend && (pm2 delete stock-frontend || true) && pm2 serve build 3001 --name stock-frontend --spa"
+spawn ssh -o StrictHostKeyChecking=no -p 14126 root@103.242.3.87 "export PATH=/usr/local/bin:\$PATH && cd /opt/stocks/frontend && (pm2 delete stock-frontend || true) && pm2 serve build 3001 --name stock-frontend --spa"
 expect {
     "*yes/no*" { send "yes\r"; exp_continue }
     "*assword:*" { send "$password\r"; exp_continue }
